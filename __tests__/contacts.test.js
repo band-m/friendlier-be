c1e36493243f823cc9253e9012eb26b2768c63a5
TravelFiend <mikegrace274@gmail.com>
2020-02-24 15:32:34 -0800

    get all contacts for a user route written tested and passing

--- __tests__/contacts.test.js
+++ __tests__/contacts.test.js
@@ -5,26 +5,28 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const User = require('../lib/models/User');
-// const Contact = require('../lib/models/Contact');
+const Contact = require('../lib/models/Contact');
 
 describe('contact routes', () => {
+  const agent = request.agent(app);
+
   beforeAll(() => connect());
 
   beforeEach(() => mongoose.connection.dropDatabase());
 
   let user;
-  // let contact;
+  let contact;
   beforeEach(async() => {
     user = await User.create({
       displayName: 'Funkadelic',
       email: 'test@test.com',
       passwordHash: 'hvjhtvut5646yrvth'
     });
-    // contact = await Contact.create({
-    //   userId: user._id,
-    //   firstName: 'George',
-    //   commFreq: '2 weeks'
-    // });
+    contact = await Contact.create({
+      userId: user._id,
+      firstName: 'George',
+      commFreq: 2
+    });
   });
 
   afterAll(() => mongoose.connection.close());
@@ -35,18 +37,56 @@ describe('contact routes', () => {
       .send({
         userId: user._id,
         firstName: 'Dingaling',
-        commFreq: '1 month'
+        commFreq: 3
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           userId: expect.any(String),
           firstName: 'Dingaling',
-          commFreq: '1 month',
+          commFreq: 3,
           connHistory: [],
           specialDates: [],
           __v: 0
         });
       });
+  });
+
+  it('should get all contacts', async() => {
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        displayName: 'Funkadelic',
+        email: 'test@test.com',
+        passwordHash: 'hvjhtvut5646yrvth'
+      });
+
+    await Contact.create({
+      userId: user._id,
+      firstName: 'Billy',
+      commFreq: 3
+    });
+
+    await agent.get(`/api/v1/contacts/${user._id}`)
+      .then(contacts => {
+        expect(contacts.body).toEqual([{
+          __v: 0,
+          _id: expect.any(String),
+          commFreq: 2,
+          connHistory: [],
+          firstName: 'George',
+          specialDates: [],
+          userId: expect.any(String)
+        }, {
+          __v: 0,
+          _id: expect.any(String),
+          commFreq: 3,
+          connHistory: [],
+          firstName: 'Billy',
+          specialDates: [],
+          userId: expect.any(String)
+        }]);
+      });
+
   });
 });
